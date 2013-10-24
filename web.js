aeb9d15da213c4b36073281ecc5335fdfe6d90df
mefuru <mehul.mandania@gmail.com>
2013-10-24 15:13:25 -0400

    refactored code for heroku test

--- web.js
+++ web.js
@@ -7,8 +7,3 @@ app.get('/', function(request, response) {
 	var data = fs.readFileSync('particle.html').toString();
 	response.send(data); // data is a string that contains the HTML data for index.html
     });
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-	console.log("Listening on " + port);
-    });
