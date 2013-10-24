32b8877804666f6ff8b0e295918e20d581d818dc
mefuru <mehul.mandania@gmail.com>
2013-10-24 15:38:11 -0400

    app.use fn applied to web.js

--- web.js
+++ web.js
@@ -3,7 +3,15 @@ var fs = require("fs");
 var app = express();
 app.use(express.logger());
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
 	var data = fs.readFileSync('particle.html').toString();
 	response.send(data); // data is a string that contains the HTML data for index.html
-    });
+});
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
